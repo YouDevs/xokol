0229010cc1c0a113b4d9ea6a62e8a5a0de340ebc
YouDevs <carlos_develops@outlook.com>
2026-04-01 17:39:08 -0600

    03: CRUD projects - upload images and create

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use App\Models\Project;
+use App\Models\Service;
 
 class ProjectController extends Controller
 {
@@ -21,9 +22,10 @@ class ProjectController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        $services = Service::all();
+        return view('admin.projects.create', compact('services'));
     }
 
     /**
@@ -31,7 +33,26 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "title" => 'required|string|max:255',
+            "description" => 'required|string|max:255',
+            'image_carousel' => 'required|image|max:5120',
+            'grid_image' => 'required|image|max:5120',
+            "grid_image_size" => 'integer',
+            "is_active" => 'boolean',
+            "service_ids" => 'nullable|array',
+            'service_ids.*' => 'integer|exists:services,id'
+        ]);
+
+        $data = $validated;
+
+        $data['image_carousel'] = $request->file('image_carousel')->store('projects/carousel', 'public');
+        $data['grid_image'] = $request->file('grid_image')->store('projects/grid', 'public');
+
+        Project::create($data);
+
+        return redirect()->route('admin.projects.index')->with('success', 'Proyecto Creado Exitosamente');
+
     }
 
     /**
